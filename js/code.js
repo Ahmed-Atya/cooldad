45c82090c0102df2b27c00631dfb24073a4d5e26
Ahmed-Atya <ahmedatya@outlook.com>
2022-03-27 17:55:20 +0200

    Cooldad landing page main file

---
+++ js/code.js
@@ -0,0 +1,6 @@
+var allCta = document.querySelector(".cta-button"),
+    overlayBg=document.querySelector(".body-overlay");
+allCta.click=
+function showModel(){
+  overlayBg.style.display="block"; 
+}
